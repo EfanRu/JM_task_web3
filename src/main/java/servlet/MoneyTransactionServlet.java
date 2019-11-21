3ecc0045b64030d51642e553cce4cbb9ea4a8392
Vyacheslav <ffgeniy@mail.ru>
2019-11-21 22:20:24 +0300

    Added code in servlet

---
+++ src/main/java/servlet/MoneyTransactionServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import model.BankClient;
+import service.BankClientService;
+import util.PageGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class MoneyTransactionServlet extends HttpServlet {
+
+    BankClientService bankClientService = new BankClientService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println(PageGenerator.getInstance().getPage("moneyTransactionPage.html", new HashMap<>()));
+        resp.setContentType("text/html;charset=utf-8");
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        bankClientService.sendMoneyToClient();
+    }
+}
---
+++ src/main/java/servlet/RegistrationServlet.java
@@ -0,0 +1,26 @@
+package servlet;
+
+import model.BankClient;
+import service.BankClientService;
+import util.PageGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class RegistrationServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println(PageGenerator.getInstance().getPage("registrationPage.html", new HashMap<>()));
+        resp.setContentType("text/html;charset=utf-8");
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //new BankClientService().addClient();
+    }
+}
